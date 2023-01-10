ab18d89bdb17e456f523647fe2fc941b46a3df90
MarceloALMoreira <90574339+MarceloALMoreira@users.noreply.github.com>
2023-01-09 23:01:02 -0300

    feat primeiro projeto classe, objeto e metedo!

---
+++ BankLine/BankLine/ContaCorrente.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankLine
+{
+
+        //Primeira classe
+    public class ContaCorrente
+    {
+        public string titular;
+        public int agencia;
+        public string conta;
+        public double saldo;
+
+
+        //primeiro metedo da classe Conta corrente, -> Metedo Depositar
+        public void  Depositar(double valor)
+        {
+            this.saldo += valor;
+        }
+
+
+    }
+
+
+}
---
+++ BankLine/BankLine/Program.cs
@@ -0,0 +1,21 @@
+﻿
+//Aqui é um objeto de conta
+using BankLine;
+
+ContaCorrente contaDoMarcelo = new ContaCorrente();
+contaDoMarcelo.titular = "Marcelo Almeida";
+contaDoMarcelo.agencia = 15;
+contaDoMarcelo.conta = "5541-F";
+contaDoMarcelo.saldo = 350;
+
+
+
+
+
+Console.WriteLine("O saldo da conta do " + contaDoMarcelo.titular + " é " + contaDoMarcelo.saldo);
+
+
+contaDoMarcelo.Depositar(200);
+
+
+Console.WriteLine("Seu novo Saldo agora é de: " + contaDoMarcelo.saldo + " R$");
